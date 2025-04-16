18d4c77281d9709bf32ba0e5de0ed4b7984b45b0
Layla1706 <nehaprabhup17@gmail.com>
2025-04-16 15:19:26 +0530

    made the main.py accept it as a fastapi application

--- Buggy_Repo/backend/main.py
+++ Buggy_Repo/backend/main.py
@@ -3,11 +3,11 @@ from routes.items import router as items_router
 from routes.analytics import router as analytics_router
 from routes.quiz import router as quiz_router
 
-app = FastAPI()
+app = FastAPI()  
 
 app.include_router(items_router, prefix="/items")
-app.include_router(analytics_router)
-app.include_router(quiz_router)
+app.include_router(analytics_router, prefix="/analytics")
+app.include_router(quiz_router, prefix="/quiz")
 
 # why the hell did I write this function?
 @app.get("/home")
